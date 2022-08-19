fd18dcc1d8c72e3d6b00e0de331cb94643beb9ff
Tyler Hopkin <tyler.hopkin12@gmail.com>
2022-08-19 13:17:58 -0500

    Added eventListeners for buttons except =

--- javascript.js
+++ javascript.js
@@ -1,3 +1,46 @@
+let num1;
+let num2;
+let operand;
+let displayText = "";
+
+const currentInput = document.querySelector('#currentInput');
+const display = document.querySelector('#display');
+const history = document.querySelector('#history');
+const numbers = document.querySelector('#numbers');
+const operators = document.querySelector('#operators');
+
+const numBtn = document.querySelector('.numBtn');
+numbers.addEventListener('click',(e)=>{
+    if(e.target.classList.contains('numBtn')){
+        displayText+=e.target.id;
+        currentInput.textContent = displayText;
+    }
+});
+
+const operator = document.querySelector('.operator');
+operators.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('operator')){
+        if(num1!=undefined){
+            num2=displayText;
+            console.log(operand, num1, num2);
+            num1 = operate(operand,num1,num2);
+            num2 = "";
+            displayText = "";
+            return;
+        }
+        num1=displayText;
+        operand = e.target.id;
+        displayText="";
+    }
+})
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', ()=>{
+    displayText = "";
+    currentInput.textContent = displayText;
+})
+
+
 function add(num1, num2){return num1+num2;}
 
 function subtract(num1, num2){return num1-num2;}
@@ -9,15 +52,15 @@ function divide(num1, num2){return num1/num2;}
 function operate(operand, num1, num2){
     switch(operand){
         case "+":
-            add(num1,num2);
+            currentInput.textContent = add(num1,num2);
             break;
         case "-":
-            subtract(num1,num2);
+            currentInput.textContent = subtract(num1,num2);
             break;
         case "*":
-            multiply(num1,num2);
+            currentInput.textContent = multiply(num1,num2);
             break;
         default:
-            divide(num1,num2);
+            currentInput.textContent = divide(num1,num2);
     }
 }
